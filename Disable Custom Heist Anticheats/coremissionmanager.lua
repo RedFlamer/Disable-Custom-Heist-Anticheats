ef350ec1bc9c634d7dc40e8392e0aeeadc903569
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2021-04-16 20:12:48 +0100

    Add files via upload

--- Disable Custom Heist Anticheats/coremissionmanager.lua
+++ Disable Custom Heist Anticheats/coremissionmanager.lua
@@ -45,7 +45,7 @@ elseif level == 'Xanax' then
 		local i = #data.elements
 
 		while i > 0 do
-			if data.elements[i].id == 100569 then
+			if data.elements[i].id == 100562 and data.elements[i].editor_name == 'EveryoneDead' then
 				table.remove(data.elements, i)
 			end
 
